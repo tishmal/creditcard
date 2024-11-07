ee0ef40288065a5fc1c977a6e6d9530a0ef34544
tishmal <arturtimur201998@gmail.com>
2024-11-07 16:19:57 +0500

    random ready 80%

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"math/rand"
 	"os"
 	"strconv"
+	"time"
 )
 
 func main() {
@@ -11,22 +14,103 @@ func main() {
 
 	flags := make(map[string]func(string))
 
-	flags["validate"] = func(value string) {
-		if Validate(value) == false {
-			fmt.Println("INCORRECT")
-		} else {
-			fmt.Println("OK")
+	var countStar int
+	var lastValueThisStar bool
+	flags["generate"] = func(value string) { // value - это значение(номер кредитной карты или нет) которое мы получаем из консоли
+		for _, ch := range value {
+			if ch == '*' {
+				countStar++
+			}
+			if len(value)-countStar >= 0 && len(value)-countStar < len(value) { // чтобы индекс не вышел из диапазона надо проверить, чтобы он был меньше длины входящей строки value
+				if value[len(value)-countStar] == '*' {
+					lastValueThisStar = true
+				} else {
+					lastValueThisStar = false
+				}
+			}
+			if countStar > 4 {
+				os.Exit(1)
+			}
+		}
+		if lastValueThisStar == false {
+			os.Exit(1)
+		}
+		if lastValueThisStar == true {
+			var digits []int
+			for i, char := range value {
+				if i >= len(value)-countStar {
+					// Генерируем случайное число от 0 до 9
+					randomNumber := randomInt(0, 10)
+
+					digits = append(digits, randomNumber)
+				} else {
+					// Преобразуем символ в цифру
+					digit, err := strconv.Atoi(string(char))
+					if err != nil {
+						return
+					}
+					digits = append(digits, digit)
+				}
+			}
+			var numCard string
+			for _, d := range digits {
+				numCard += strconv.Itoa(d)
+			}
+			fmt.Println(numCard)
 		}
 	}
-	// Обработка аргументов
-	for i := 1; i < len(args); i++ {
-		if args[i] == "validate" && i+1 < len(args) {
-			// Проверяем все аргументы после ключа "validate"
-			for j := i + 1; j < len(args); j++ {
-				flags["validate"](args[j]) // Проверяем каждый номер карты
-			}
-			return // Завершаем выполнение программы после обработки всех карт
+
+	var hasError bool
+
+	flags["validate"] = func(value string) {
+		if Validate(value) == false {
+			fmt.Fprintf(os.Stderr, "INCORRECT\n")
+			hasError = true
+		} else {
+			fmt.Fprintf(os.Stdout, "OK\n")
 		}
+	}
+	// Обработка флагов и аргументов
+	var stdinInput bool
+	for i := 1; i < len(args); i++ {
+		if args[i] == "--stdin" {
+			stdinInput = true
+		}
+	}
+
+	if stdinInput {
+		// Если флаг --stdin активирован, читаем номера карт с stdin
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			// Читаем строки с картами и передаем их в функцию валидации
+			cardNumber := scanner.Text()
+			flags["validate"](cardNumber)
+		}
+		if err := scanner.Err(); err != nil {
+			fmt.Fprintf(os.Stderr, "INCORRECT\n")
+			hasError = true
+		}
+	} else {
+		// Обработка аргументов командной строки
+		for i := 1; i < len(args); i++ {
+			if args[i] == "validate" && i+1 < len(args) {
+				// Проверяем все аргументы после ключа "validate"
+				for j := i + 1; j < len(args); j++ {
+					flags["validate"](args[j]) // Проверяем каждый номер карты
+				}
+				return // Завершаем выполнение программы после обработки всех карт
+			}
+			if args[i] == "generate" && i+1 < len(args) {
+				flags["generate"](args[2])
+				return
+			}
+		}
+	}
+	// после завершения всех проверок - конец  завершения
+	if hasError {
+		os.Exit(1)
+	} else {
+		os.Exit(0)
 	}
 }
 
@@ -36,6 +120,10 @@ func Validate(numberCard string) bool {
 	// Создадим слайс для хранения цифр
 	var digits []int
 	var validCard bool
+
+	if len(numberCard) < 13 || len(numberCard) > 19 {
+		validCard = false
+	}
 
 	// Перебираем каждый символ строки, преобразуем его в число и добавляем в слайс
 	for _, char := range numberCard {
@@ -71,6 +159,15 @@ func Validate(numberCard string) bool {
 	} else {
 		validCard = false
 	}
+	if numberCard == "" {
+		validCard = false
+	}
 
 	return validCard
+}
+
+func randomInt(min, max int) int {
+	//
+	rand.Seed(time.Now().UnixNano())
+	return rand.Intn(max-min) + min // Генерируем случайное число в диапазоне [min, max)
 }
