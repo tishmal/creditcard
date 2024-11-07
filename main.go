0fb62f53c126cb59178c946813d529b04ed1e6dd
tishmal <arturtimur201998@gmail.com>
2024-11-07 18:08:38 +0500

    generate ready 95%

--- main.go
+++ main.go
@@ -38,8 +38,28 @@ func main() {
 		}
 		if lastValueThisStar == true {
 			var digits []int
+			var validNumbers []string
 			for i, char := range value {
 				if i >= len(value)-countStar {
+					maxCombinations := 1
+					for i := 0; i < countStar; i++ {
+						maxCombinations *= 10
+					}
+
+					for i := 0; i < maxCombinations; i++ {
+						// Форматируем число с ведущими нулями
+						replacement := fmt.Sprintf("%0*d", countStar, i)
+						// Создаем текущий номер, заменяя звездочки на цифры
+						currentNumber := value[:len(value)-countStar] + replacement
+
+						if Validate(currentNumber) {
+							validNumbers = append(validNumbers, currentNumber)
+						}
+					}
+					if len(validNumbers) == 0 {
+						os.Exit(1)
+					}
+
 					// Генерируем случайное число от 0 до 9
 					randomNumber := randomInt(0, 10)
 
@@ -57,7 +77,10 @@ func main() {
 			for _, d := range digits {
 				numCard += strconv.Itoa(d)
 			}
-			fmt.Println(numCard)
+			// fmt.Println(numCard)
+			for _, num := range validNumbers {
+				fmt.Println(num)
+			}
 		}
 	}
 
